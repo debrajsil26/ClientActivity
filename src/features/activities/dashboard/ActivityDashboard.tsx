ac5fb7ce5cf3eb397d184d2832fc33e0e9744b42
Debraj <debrajsil@gmail.com>
2026-04-27 12:42:25 +0530

    Add ActivityDetails

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,16 +1,20 @@
 import { Grid  } from "@mui/material"
 import ActivityList from "./ActivityList";
+import ActivityDetail from "../details/ActivityDetails.tsx";
 type Props = {
     activities: Activity[]
 }
 export default function ActivityDashboard(activitiesProp : Props) {
     const { activities } = activitiesProp;
     return (
-        <Grid container>
-            <Grid size={9}>
-                <ActivityList activities={activities} />
-            </Grid>
-
+         
+       <Grid container spacing={3}>
+        <Grid size={7}>
+            <ActivityList activities={activities} />
         </Grid>
+        <Grid size={5}>
+            {activities[0] && <ActivityDetail activity={activities[0]} />}
+        </Grid>
+    </Grid>
     )
 }
